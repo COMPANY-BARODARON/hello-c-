4e0638b45adca40eec1655f63f71a132634ef9e8
OYATULO <54220399+OYATULO@users.noreply.github.com>
2021-08-17 15:11:46 +0500

    Create hello.cs

---
+++ hello.cs
@@ -0,0 +1,17 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Runtime.Serialization.Formatters.Binary;
+namespace Console_for_lessons
+{ 
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+       Console.Write("hello world !");
+            Console.ReadKey();
+        }
+    }
+}
+ 
